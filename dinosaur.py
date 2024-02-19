1f770fb4c613e7e6912f011aee9755db35347fac
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2024-02-19 17:19:07 +0530

    Update dinosaur.py

--- dinosaur.py
+++ dinosaur.py
@@ -1,186 +1,130 @@
+import matplotlib.pyplot as plt
+from moviepy.editor import ImageSequenceClip
+from PIL import ImageDraw
 import numpy as np
-from PIL import Image , ImageDraw
+from copy import deepcopy
 
-def fig_to_np_array(fig):
-    '''
-    Convert Figure to Numpy Array
+from helper import (
+    get_png , 
+    map_to_pixels , 
+    get_pixels
+)
 
-    Args :
+import numpy as np
+
+def lineplot(x_data , y_data ,
+             linewidth = 3 , beautify = True ,
+             figsize = (10 , 6) , dpi = 72 , fps = 25 ,
+             right_pad = 0 , left_pad = 0 , up_pad = 0 , down_pad = 0 ,
+             rgb = (255 , 255 , 255)) :
     
-        1) fig : Figure
-            Figure to Convert
-
-    Returns :
-    
-        1) buf : Numpy Array
-            Numpy Array of the Figure
     '''
-    
-    fig.canvas.draw()
+    Line Plot Animation Function for Dinosaur
 
-    w, h = fig.canvas.get_width_height()
-    buf = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8)
-    buf.shape = (h, w, 3)
+    Args : 
 
-    return buf
+        1) x_data : Iterable  
+            X - Axis Data
 
-def norm(value , max_value , min_value) : 
-    '''
-    Normalize Value
+        2) y_data : Iterable
+            Y - Axis Data
 
-    Args :
+        3) linewidth : int
+            Width of the Axes Line
 
-        1) value : Float
-            Value to Normalize
+        4) beautify : bool
+            If True , Beautify the Plot
 
-        2) max_value : Float
-            Maximum Value
+        5) figsize : Tuple
+            Size of the Figure
 
-        3) min_value : Float
-            Minimum Value
-
-    Returns :
-
-        1) norm_value : Float
-    '''
-
-    return (value - min_value) / (max_value - min_value)
-
-def get_png(figure) :
-    '''
-    Get PNG from Figure
-
-    Args :
-
-        1) figure : Figure
-            Figure to Convert
-
-    Returns :
-
-        1) image : Image
-            Image of the Figure
-    '''
-
-    figure = fig_to_np_array(figure)
-    image = Image.new('RGB' , (figure.shape[1] , figure.shape[0]))
-    draw = ImageDraw.Draw(image)
-
-    for row in range(figure.shape[0]) :
-
-        for col in range(figure.shape[1]) :
-
-            if figure[row][col][0] != 0 : draw.point((col , row) , fill = (figure[row][col][0] , figure[row][col][1] , figure[row][col][2]))
-
-    return image
-
-def map_to_pixels(points , width , height , width_pad , height_pad , dpi) :
-    '''
-    Map Points to Pixels
-
-    Args :
-
-        1) points : List
-            List of Points
-        
-        2) width : Int
-            Width of the Image
-
-        3) height : Int
-            Height of the Image
-
-        4) width_pad : Int
-            Width Padding
-
-        5) height_pad : Int
-            Height Padding
-
-        6) dpi : Int
+        6) dpi : int
             Dots per Inch
-    '''
 
-    x_points = [x for x , _ in points]
-    y_points = [y for _ , y in points]
-    x_max = max(x_points)
-    y_max = max(y_points)
-    x_min = min(x_points)
-    y_min = min(y_points)
+        7) fps : int
+            Frames per Second
 
-    x_max += 0.005 * x_max * dpi
-    y_max += 0.005 * y_max * dpi
+        8) right_pad : int
+            Width to the Right of the Point
 
-    pixel_points = []
+        9) left_pad : int
+            Width to the Left of the Point
 
-    for x , y in points :
+        10) up_pad : int
+            Width to the Up of the Point
 
-        x_norm = norm(x , x_max , x_min)
-        y_norm = norm(y , y_max , y_min)
+        11) down_pad : int 
+            Width to the Down of the Point
 
-        x_scale = x_norm * width
-        y_scale = y_norm * height
-
-        y_scale = height - y_scale # PIL starts from top-left 
-
-        x_scale += width_pad
-        y_scale -= height_pad
-
-        pixel_points.append((
-            x_scale , 
-            y_scale 
-))
-
-    return pixel_points
-
-def get_pixels(point , 
-               right_pad , left_pad , 
-               up_pad , down_pad) : 
-    '''
-    Get Pixels
-
-    Args :
-
-        1) point : Tuple
-            Point
-
-        2) right_pad : Int
-            Right Padding
-
-        3) left_pad : Int
-            Left Padding
-
-        4) up_pad : Int
-            Up Padding
-
-        5) down_pad : Int
-            Down Padding
-
+        12) rgb : Tuple
+            RGB Color of the Line
+        
     Returns :
     
-            1) pixels : List
-                List of Pixels
+            1) Animation
+                Line Plot Animation
     '''
 
-    pixels = [point]
+    figure , axes = plt.subplots(figsize = figsize , dpi = dpi)
 
-    for padding in range(right_pad + 1) : pixels.append((point[0] + padding , point[1]))
-    for padding in range(left_pad + 1) : pixels.append((point[0] - padding , point[1]))
-    for padding in range(up_pad + 1) : pixels.append((point[0] , point[1] + padding))
-    for padding in range(down_pad + 1) : pixels.append((point[0] , point[1] - padding))
+    if beautify :
+        axes.spines['right'].set_visible(False)
+        axes.spines['top'].set_visible(False)
 
-    x_set = set([
-        pixel[0]
-        for pixel
-        in pixels
-    ])
-    y_set = set([
-        pixel[1]
-        for pixel
-        in pixels
-    ])
+    axes.spines['left'].set_linewidth(linewidth)
+    axes.spines['bottom'].set_linewidth(linewidth)
 
-    pixels = [
-        (x_cor , y_cor)
-        for x_cor in x_set
-        for y_cor in y_set
+    axes.spines['left'].set_position('zero')
+    axes.spines['bottom'].set_position('zero')
+
+    axes.xaxis.set_ticks_position('bottom')
+    axes.yaxis.set_ticks_position('left')
+
+    figure.set_facecolor('black')
+    axes.set_facecolor('black')
+
+    axes.spines['left'].set_color('white')
+    axes.spines['bottom'].set_color('white')
+
+    axes.tick_params(colors='white')
+
+    axes.set_xlim(0 , max(x_data))
+    axes.set_ylim(0 , max(y_data))
+
+    points = [
+        (x , y)
+        for x , y
+        in zip(x_data , y_data)
     ]
 
-    return pixels
+    height = (figsize[0]) * dpi
+    width = (figsize[1]) * dpi
+
+    points = map_to_pixels(points , height , width , width_pad = (dpi * figsize[0]) / 7.2 , height_pad = (figsize[1] * dpi) / 8.64 , dpi = dpi)
+    plane_mask = get_png(figure)
+
+    anim_array = [plane_mask]
+
+    for point in points  :
+
+        temp_plane_mask = deepcopy(plane_mask)
+
+        draw = ImageDraw.Draw(temp_plane_mask)
+
+        pixels = get_pixels(point , right_pad , left_pad , up_pad , down_pad)
+
+        for point in pixels : draw.point(point , fill = (rgb[0] , rgb[1] , rgb[2]))
+
+        anim_array.append(temp_plane_mask)
+
+        plane_mask = deepcopy(temp_plane_mask)
+
+    anim_array = [np.array(val) for val in anim_array]
+
+    images = [np.uint8(frame[..., ::-1]) for frame in anim_array]
+
+    clip = ImageSequenceClip(images, fps=fps)
+
+    del images , anim_array
+
+    return clip.ipython_display(fps=10, autoplay=True, loop=True)
